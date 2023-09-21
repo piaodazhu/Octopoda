1729a9e0596aebdb8eabec2d93743db5fdbb66df
piaodazhu <2283079466@qq.com>
2023-09-21 20:35:09 +0800

    fix ssh login: let master keeping the ssh info

--- brain/api/sshProxy.go
+++ brain/api/sshProxy.go
@@ -18,12 +18,30 @@ type proxyMsg struct {
 	Data string
 }
 
+func SshLoginInfo(ctx *gin.Context) {
+	name := ctx.Query("name")
+	if info, found := network.GetSshInfo(name); found {
+		ctx.JSON(200, info)
+		return 
+	}
+	ctx.JSON(404, struct{}{})
+}
+
 func SshRegister(ctx *gin.Context) {
-	proxyCmd(ctx, ctx.PostForm("name"), message.TypeSshRegister)
+	name := ctx.PostForm("name")
+	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
+	if len(name) == 0 || len(username) == 0 {
+		ctx.JSON(400, struct{}{})
+		return
+	}
+	network.CreateSshInfo(name, username, password)
+	proxyCmd(ctx, name, message.TypeSshRegister)
 }
 
 func SshUnregister(ctx *gin.Context) {
-	proxyCmd(ctx, ctx.Query("name"), message.TypeSshUnregister)
+	name := ctx.Query("name")
+	proxyCmd(ctx, name, message.TypeSshUnregister)
 }
 
 func proxyCmd(ctx *gin.Context, name string, cmdType int) {
--- brain/network/proxy.go
+++ brain/network/proxy.go
@@ -5,6 +5,7 @@ import (
 	"brain/logger"
 	"brain/message"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/piaodazhu/proxylite"
@@ -28,24 +29,26 @@ func InitProxyServer() {
 	}
 
 	proxyliteServer.OnTunnelCreated(func(ctx *proxylite.Context) {
-		entry := &message.RegisterParam{
-			Type:        "other",
-			Name:        ctx.ServiceInfo().Name,
-			Ip:          octlFaceIp,
-			Port:        int(ctx.ServiceInfo().Port),
-			Description: ctx.ServiceInfo().Message,
-			TTL:         0,
-		}
-		err := nameRegister(entry)
-		if err != nil {
-			logger.Exceptions.Printf("add proxylite service %s failed: %v", entry.Name, err)
-		}
+		// entry := &message.RegisterParam{
+		// 	Type:        "other",
+		// 	Name:        ctx.ServiceInfo().Name,
+		// 	Ip:          octlFaceIp,
+		// 	Port:        int(ctx.ServiceInfo().Port),
+		// 	Description: ctx.ServiceInfo().Message,
+		// 	TTL:         0,
+		// }
+		// err := nameRegister(entry)
+		// if err != nil {
+		// 	logger.Exceptions.Printf("add proxylite service %s failed: %v", entry.Name, err)
+		// }
+		CompleteSshInfo(ctx.ServiceInfo().Name, octlFaceIp, ctx.ServiceInfo().Port)
 	})
 	proxyliteServer.OnTunnelDestroyed(func(ctx *proxylite.Context) {
-		err := nameUnregister(ctx.ServiceInfo().Name)
-		if err != nil {
-			logger.Exceptions.Printf("delete proxylite service %s failed: %v", ctx.ServiceInfo().Name, err)
-		}
+		// err := nameUnregister(ctx.ServiceInfo().Name)
+		// if err != nil {
+		// 	logger.Exceptions.Printf("delete proxylite service %s failed: %v", ctx.ServiceInfo().Name, err)
+		// }
+		DelSshInfo(ctx.ServiceInfo().Name)
 	})
 	go func() {
 		err := proxyliteServer.Run(fmt.Sprintf("0.0.0.0:%d", config.GlobalConfig.ProxyliteServer.Port))
@@ -80,3 +83,51 @@ func InitProxyServer() {
 		}
 	}()
 }
+
+type SSHInfo struct {
+	Ip       string
+	Port     uint32
+	Username string
+	Password string
+}
+
+var sshInfos sync.Map
+
+func init() {
+	sshInfos = sync.Map{}
+}
+
+func CreateSshInfo(name string, username, password string) {
+	sshInfos.Store(name, SSHInfo{
+		Username: username,
+		Password: password,
+	})
+}
+
+func CompleteSshInfo(name string, ip string, port uint32) {
+	if info, found := GetSshInfo(name); found {
+		info.Ip = ip
+		info.Port = port
+		sshInfos.Store(name, info)
+	}
+}
+
+func DelSshInfo(name string) {
+	sshInfos.Delete(name)
+}
+
+func GetSshInfo(name string) (SSHInfo, bool) {
+	if v, found := sshInfos.Load(name); found {
+		info := v.(SSHInfo)
+		if len(info.Username) == 0 {
+			DelSshInfo(name)
+			return SSHInfo{}, false
+		}
+		return v.(SSHInfo), true
+	}
+	return SSHInfo{}, false
+}
+
+// func AddSshInfo(name string, info SSHInfo) {
+// 	sshInfos.Store(name, info)
+// }
