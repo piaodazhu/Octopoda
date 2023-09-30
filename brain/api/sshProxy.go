4a100c9032c65087dba5a28dff4b172a5ba528ff
piaodazhu <2283079466@qq.com>
2023-09-30 18:53:58 +0800

    fix bug: sshinfo duplicate

--- brain/api/sshProxy.go
+++ brain/api/sshProxy.go
@@ -32,49 +32,95 @@ func SshRegister(ctx *gin.Context) {
 	username := ctx.PostForm("username")
 	password := ctx.PostForm("password")
 	if len(name) == 0 || len(username) == 0 {
-		ctx.JSON(400, struct{}{})
+		ctx.JSON(400, proxyMsg{
+			Code: -1,
+			Msg: "ERR",
+			Data: "invalid arguments",
+		})
 		return
 	}
+	services, err := network.ProxyServices()
+	if err != nil {
+		ctx.JSON(404, proxyMsg{
+			Code: -1,
+			Msg: "ERR",
+			Data: fmt.Sprintf("cannot get proxy services: %s", err.Error()),
+		})
+		return
+	}
+
+	// 存在一致性问题，但是危害不大
+	for _, s := range services {
+		if name == s.Name {
+			ctx.JSON(400, proxyMsg{
+				Code: -1,
+				Msg: "ERR",
+				Data: fmt.Sprintf("service %s already exists", name),
+			})
+			return
+		}
+	}
 	network.CreateSshInfo(name, username, password)
-	proxyCmd(ctx, name, message.TypeSshRegister)
+	if !proxyCmd(ctx, name, message.TypeSshRegister) { // 不成功就删除
+		network.DelSshInfo(name)
+	}
 }
 
 func SshUnregister(ctx *gin.Context) {
 	name := ctx.Query("name")
-	proxyCmd(ctx, name, message.TypeSshUnregister)
+	if proxyCmd(ctx, name, message.TypeSshUnregister) { // 成功就删除
+		network.DelSshInfo(name)
+	}
 }
 
-func proxyCmd(ctx *gin.Context, name string, cmdType int) {
+func proxyCmd(ctx *gin.Context, name string, cmdType int) bool {
 	if name == "master" {
 		ip, _ := network.GetOctlFaceIp()
+		if cmdType == message.TypeSshRegister {
+			network.CompleteSshInfo(name, ip, uint32(config.GlobalConfig.OctlFace.SshPort))
+		}
 		ctx.JSON(200, proxyMsg{
 			Code: 0,
 			Msg: "OK",
 			Data: fmt.Sprintf("%s:%d", ip, config.GlobalConfig.OctlFace.SshPort),
 		})
-		return
+		return true
 	}
 	if state, ok := model.GetNodeState(name); !ok || state != model.NodeStateReady {
-		ctx.JSON(404, struct{}{})
-		return
+		ctx.JSON(404, proxyMsg{
+			Code: -1,
+			Msg: "ERR",
+			Data: fmt.Sprintf("node %s not found", name),
+		})
+		return false
 	}
 	raw, err := model.Request(name, cmdType, []byte{})
 	if err != nil {
 		logger.Comm.Println(message.MsgTypeString[cmdType], err)
-		ctx.JSON(500, struct{}{})
-		return
+		ctx.JSON(500, proxyMsg{
+			Code: -1,
+			Msg: "ERR",
+			Data: fmt.Sprintf("master request %s error", name),
+		})
+		return false
 	}
 
 	pmsg := proxyMsg{}
 	err = json.Unmarshal(raw, &pmsg)
 	if err != nil {
 		logger.Comm.Println("proxyMsg Unmarshal", err)
-		ctx.JSON(500, struct{}{})
-		return
+		ctx.JSON(500, proxyMsg{
+			Code: -1,
+			Msg: "ERR",
+			Data: fmt.Sprintf("proxyMsg Unmarshal error: %s", err),
+		})
+		return false
 	}
 	if pmsg.Code == 0 {
 		ctx.JSON(200, pmsg)
+		return true
 	} else {
 		ctx.JSON(500, pmsg)
+		return false
 	}
 }
