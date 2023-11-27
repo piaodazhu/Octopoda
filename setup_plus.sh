091b7f78265a77b889f29c4dbe165a50d37d75a1
piaodazhu <2283079466@qq.com>
2023-11-27 09:46:18 +0800

    improve setup_plus.sh for pakma

--- setup_plus.sh
+++ setup_plus.sh
@@ -21,7 +21,7 @@ fi
 
 NAME=$1
 INSTALL_ARCH=arm
-INSTALL_VERSION=v1.6.5
+INSTALL_VERSION='1.7.1'
 OUTPUT_DIR=./installers/
 
 HTTPNS_URL=https://10.108.30.85:3455/release
@@ -35,9 +35,9 @@ cd ${WORK_DIR} && bash ./CertGen.sh client $NAME && mv ca.pem $NAME && rm -rf ca
 
 # fetch installation package
 wget --ca-certificate=$NAME/ca.pem --certificate=$NAME/client.pem --private-key=$NAME/client.key \
-	${HTTPNS_URL}/tentacle_${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz || (rm -rf ${WORK_DIR} && exit 1)
+	${HTTPNS_URL}/tentacle_v${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz || (rm -rf ${WORK_DIR} && exit 1)
 wget --ca-certificate=$NAME/ca.pem --certificate=$NAME/client.pem --private-key=$NAME/client.key \
-	${HTTPNS_URL}/pakma_${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz || (rm -rf ${WORK_DIR} && exit 1)
+	${HTTPNS_URL}/pakma_v${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz || (rm -rf ${WORK_DIR} && exit 1)
 
 # write installation script
 cat > install.sh << EOF
@@ -45,14 +45,16 @@ if [ "\$(id -u)" != "0" ]; then
    echo "You must run this script as root" 1>&2
    exit 1
 fi
-tar -Jxvf tentacle_${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz && \\
-	cd tentacle_${INSTALL_VERSION}_linux_${INSTALL_ARCH} && \\
+tar -Jxvf tentacle_v${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz && \\
+	cd tentacle_v${INSTALL_VERSION}_linux_${INSTALL_ARCH} && \\
 	sed -i "s/pi0/$NAME/" tentacle.yaml && bash setup.sh && \\
 	echo "setup tentacle done" || exit 1
 cd ../ && mkdir -p /etc/octopoda/cert/ && cp $NAME/ca.pem $NAME/client.key $NAME/client.pem /etc/octopoda/cert/ && \\
 	echo "copy cert and key done" || exit 1
-tar -Jxvf pakma_${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz && \\
-	cd pakma_${INSTALL_VERSION}_linux_${INSTALL_ARCH} && bash setup.sh tentacle && \\
+mkdir -p /var/octopoda/tentacle/pakma/ && cp tentacle_v${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz /var/octopoda/tentacle/pakma/ && \\
+	echo '{"StateType":2,"Version1":"${INSTALL_VERSION}","Version2":"${INSTALL_VERSION}","Version3":""}' > /var/octopoda/tentacle/pakma/pakma.json || exit 1
+tar -Jxvf pakma_v${INSTALL_VERSION}_linux_${INSTALL_ARCH}.tar.xz && \\
+	cd pakma_v${INSTALL_VERSION}_linux_${INSTALL_ARCH} && bash setup.sh tentacle && \\
 	echo "setup pakma_tentacle done" || exit 1
 echo "DONE"
 EOF
